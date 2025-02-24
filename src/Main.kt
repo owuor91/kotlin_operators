75567ae2e9a44fe4f5963c58287246e46f869f8d
Meron T. Gebru <meronteklehaymanotgebru@gmail.com>
2025-02-24 18:40:09 +0300

    Added Equality operators

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,6 @@
 fun main() {
-    var num1= 50.0
-    var num2=30
-    var result=num1+num2
-    var sub=num1-num2
-    var mul=num1*num2
-    var div=num1/num2
-    var rem=num1%num2
-    println(result)
-    println(sub)
-    println(mul)
-    println(div)
-    println(rem)
+    var a=65
+    var b=32
+    println(a!=b)
 
 }
