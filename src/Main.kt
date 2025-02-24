c2bc7351e74da3928e12323dc45869bc62290699
wangara <mercywangarakwagalakwe@gmail.com>
2025-02-24 18:40:38 +0300

    add increament,decreament,comparison and equality operators

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,6 @@
 fun main() {
-    println("Hello World!")
-    var num1 = 50
-    var num2 = 30
-    var sum = num1+num2
-    println(sum)
-    var result = num1%num2
-    println(result)
+
+            var a = 65
+    var b = 32
+    println(a!=b)
 }
