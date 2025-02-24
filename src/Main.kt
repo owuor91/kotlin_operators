369900d1fb9cf99c428444c003528603e2fbb3bb
birhanu <birhanutbelay@gmail.com>
2025-02-24 18:20:30 +0300

    introduce arithmetic operators

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,14 @@
 fun main() {
-    println("Hello World!")
+    var num1= 50.0
+    var num2= 30
+    var sum= num1 + num2
+    println(sum)
+    var difference= num1-num2
+    println(difference)
+    var product= num1 * num2
+    println(product)
+    var division=num1/num2
+    println(division)
+    var modulus = num1%num2
+    println(modulus)
 }
