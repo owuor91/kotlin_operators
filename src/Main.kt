c4f2e863a582f4f93a980cbce3d313c02a77b2c1
Meron T. Gebru <meronteklehaymanotgebru@gmail.com>
2025-02-24 18:20:08 +0300

    Used diffrent types of arithmetic operators

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,15 @@
 fun main() {
-    println("Hello World!")
+    var num1= 50.0
+    var num2=30
+    var result=num1+num2
+    var sub=num1-num2
+    var mul=num1*num2
+    var div=num1/num2
+    var rem=num1%num2
+    println(result)
+    println(sub)
+    println(mul)
+    println(div)
+    println(rem)
+
 }
