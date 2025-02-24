cdc867c4c54e4ded56362f35595f8aa2828ddff2
birhanu <birhanutbelay@gmail.com>
2025-02-24 18:40:42 +0300

    add increament, decreament, and equality operators

--- src/Main.kt
+++ src/Main.kt
@@ -11,4 +11,17 @@ fun main() {
     println(division)
     var modulus = num1%num2
     println(modulus)
+    var a=4
+    var b= 5
+    a+=b
+    println(a)
+    var num3= 20
+    num3++
+    println(num3)
+    num3--
+    println(num3)
+    var d =65
+    var e= 32
+    println(d!=e)
+
 }
