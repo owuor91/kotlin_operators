eb6198977c6dec092ea8eed037ad82d25097a7bc
A <Arsema.arsemaagebremichael@gmail.com>
2025-02-24 18:41:47 +0300

    add increment,decrement, comparison and equality

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,9 @@
 fun main() {
-    var num1 = 50.0
+   /* var num1 = 50.0
     var num2 = 30
     var result = num1 % num2
-    println(result)
+    println(result)*/
+    var a = 65
+    var b = 32
+    println(b == a)
 }
